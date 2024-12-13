79d83041b554e8c73ff5bf0348d2a08204bd0cee
Sarah Mckee <sarahmckee714@gmail.com>
2024-12-12 21:57:58 -0600

    feat: added menu shebang, query variable, and menu function

---
+++ element.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+MENU () {
+  # ask for user input
+  # read user input
+  # query database
+}
+
+MENU
